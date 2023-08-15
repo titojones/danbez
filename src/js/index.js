97bfa3b33cb2c9bdae4c4cd0e9081c468bdc9765
Jhonas Lalo <titojonesisreal@gmail.com>
2023-08-16 00:43:34 +0800

    added onclick hover

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,23 @@
 const menuToggler = document.querySelector(".menutoogler");
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".--mobile-wrapper");
-
+const menuLinks = document.querySelectorAll(".--web-menu a");
+let activeLink = null;
 const toggleMenu = () => {
   hamburger.classList.toggle("open");
   mobileMenu.classList.toggle("active");
 };
 
 menuToggler.addEventListener("click", toggleMenu);
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (activeLink) {
+      activeLink.classList.remove("active");
+    }
+
+    link.classList.add("active");
+    activeLink = link;
+  });
+});
